16cae773e5aa5f1edeaf07cf1ca8dcb5775f6605
Sidrah-Tola <sidrahtola@gmail.com>
2024-12-09 14:58:09 +0530

    Login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../hooks/useLogin'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  // console.log(email, password)
+
+  const{login, loading} = useLogin()
+  
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+
+    await login(email, password)
+  }
+  
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-200'>
@@ -10,13 +24,19 @@ const Login = () => {
           <span className='text-blue-700'> ChatSphere</span>
           </h1>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label className="label p-2">
                 <span className='text-base label-text'>Email</span>
               </label>
 
-              <input type="text" placeholder='Enter your email' className='w-full input input-bordered h-10' />
+              <input 
+              type="text" 
+              placeholder='Enter Email' 
+              className='w-full input input-bordered h-10'
+              value={email} 
+              onChange={(e)=>setEmail(e.target.value)}
+              />
             </div>
 
             <div>
@@ -24,14 +44,26 @@ const Login = () => {
                 <span className='text-base label-text'>Password</span>
               </label>
 
-              <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-10' />
+              <input 
+              type="password" 
+              placeholder='Enter Password' 
+              className='w-full input input-bordered h-10'
+              value={password} 
+              onChange={(e)=>setPassword(e.target.value)} 
+              />
             </div>
 
             <Link to={"/signup"} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
             {"Don't"} have an account?</Link>
 
             <div>
-              <button className='btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white'>Login</button>
+              <button className='btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white' disabled={loading} >
+                {loading ? (
+                  <span className='loading loading-spinner'></span>
+                ):(
+                  "Login"
+                )}
+              </button>
             </div>
           </form>      
       </div>
